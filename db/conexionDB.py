b4f43ecd3744fe5359e00ea647d3b3a3177b23b7
JMSilvaMolina <juanmanuel.silvamolina@gmail.com>
2024-12-14 20:37:12 -0300

    bd

--- db/conexionDB.py
+++ db/conexionDB.py
@@ -9,4 +9,9 @@ def connect_DB():
         password="postgres"
     )
     cursor = conn.cursor()
-    
+    cursor.execute("CREATE TABLE IF NOT EXISTS usuarios ( id SERIAL PRIMARY KEY, username VARCHAR(50) UNIQUE NOT NULL, password VARCHAR(50) NOT NULL)")
+    cursor.execute(''' CREATE TABLE IF NOT EXISTS clientes ( id SERIAL PRIMARY KEY, nombre VARCHAR(100) NOT NULL ) ''')
+    cursor.execute(''' CREATE TABLE IF NOT EXISTS ventas ( id SERIAL PRIMARY KEY, cliente_id INTEGER REFERENCES clientes(id), monto DECIMAL NOT NULL ) ''')
+    cursor.execute(sql.SQL(""" INSERT INTO usuarios (username, password) VALUES (%s, %s) ON CONFLICT (username) DO NOTHING """), ("admin", "admin"))
+    conn.commit()
+    conn.close()
